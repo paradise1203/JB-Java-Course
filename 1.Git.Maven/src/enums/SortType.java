e8169a999b9667df359755c1a80c765c27d3b727
Aidar <aidarshaifutdinov@gmail.com>
2016-11-09 16:35:28 +0300

    Preparation for lesson 1

---
+++ 1.Git.Maven/src/enums/SortType.java
@@ -0,0 +1,8 @@
+package enums;
+
+/**
+ * @author Aidar Shaifutdinov.
+ */
+public enum SortType {
+    MERGE_SORT, BUBBLE_SORT
+}
---
+++ 1.Git.Maven/src/exception/SortingFailedException.java
@@ -0,0 +1,11 @@
+package exception;
+
+/**
+ * @author Aidar Shaifutdinov.
+ */
+public class SortingFailedException extends RuntimeException {
+
+    public SortingFailedException(String message) {
+        super(message);
+    }
+}
