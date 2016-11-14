dfe58138788d3d86c7bcf7a9d0ad2eccb01a20ef
Alexander Pliskin <alexnetmore@gmail.com>
2016-11-14 14:24:46 +0300

    [2.DesignPattern] [HotFix] pom updated

--- 2.DesignPatterns/src/main/java/com/kpfu/itis/decorator/Main.java
+++ 2.DesignPatterns/src/main/java/com/kpfu/itis/decorator/Main.java
@@ -7,7 +7,7 @@ import java.io.InputStream;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    File file = new File("lesson2.DesignPatterns/src/main/resources/file.txt");
+    File file = new File("2.DesignPatterns/src/main/resources/file.txt");
     InputStream is = new FileInputStream(file);
     int c;
     while ((c = is.read()) != -1) {
