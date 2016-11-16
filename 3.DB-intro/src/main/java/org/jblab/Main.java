217cb5e2e5fa381a38ca8f03409f4adc7822ef48
paradise1203 <aidarshaifutdinov@gmail.com>
2016-11-16 15:20:06 +0300

    Added class work

--- 3.DB-intro/src/main/java/org/jblab/Main.java
+++ 3.DB-intro/src/main/java/org/jblab/Main.java
@@ -1,9 +1,15 @@
 package org.jblab;
 
+import org.jblab.dao.UserDAO;
+
+import java.sql.SQLException;
+
 /**
  * @author Aidar Shaifutdinov.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        UserDAO userDAO = new UserDAO();
+        System.out.println(userDAO.getAll());
     }
 }
