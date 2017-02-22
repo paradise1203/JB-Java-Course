70e4c2e83188f6b0ed2c5e74e6b1a311c0e10838
paradise1203 <aidarshaifutdinov@gmail.com>
2017-02-22 12:08:49 +0300

    Preparation for lesson 11 vol.2

--- 11.Spring-DB/src/main/java/org/jblab/util/UserForm.java
+++ 11.Spring-DB/src/main/java/org/jblab/util/UserForm.java
@@ -1,41 +1,27 @@
-package org.jblab.model;
+package org.jblab.util;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
 import org.jblab.model.enums.Sex;
 
-import javax.persistence.*;
+import javax.validation.constraints.Size;
 
 /**
  * @author Aidar Shaifutdinov.
  */
-@Entity
-//NOTE Вот эта аннотация задает имя таблицы, из-за этого не работало
-@Table(name = "users")
-public class User {
+public class UserForm {
 
     private static final String BLANK_MESSAGE = "This field is mandatory";
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
+    @NotBlank(message = BLANK_MESSAGE)
+    @Email
     private String email;
 
+    @NotBlank(message = BLANK_MESSAGE)
+    @Size(min = 7, max = 20, message = "Password should be from 7 to 20 symbols")
     private String password;
 
-    @Enumerated(EnumType.STRING)
     private Sex sex;
-
-    public static String getBlankMessage() {
-        return BLANK_MESSAGE;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getEmail() {
         return email;
